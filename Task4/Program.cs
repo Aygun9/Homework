7ffb58c3f7452830d0b7d7ef8e216a9c60206db0
Aygun <aygun.gasimova@inbox.ru>
2022-11-26 15:59:50 +0400

    Task 4 completed

---
+++ Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Insert number1");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Insert number2");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Insert number3");
+int number3 = int.Parse(Console.ReadLine());
+int max = number1  ;
+if(number1 > max) max = number1 ;
+if(number2 > max) max = number2 ;
+if(number3 > max) max = number3;
+Console.WriteLine(max);
